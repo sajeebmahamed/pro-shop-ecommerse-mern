9f02c8837e2ad98f90f8553585d78a3bfe06d7fa
Sajeeb <sajeebahamed6@gmail.com>
2020-12-19 02:43:08 +0600

    get all products from redux

--- frontend/src/screens/HomeScreen/HomeScreen.js
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -1,29 +1,31 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../../actions/productActions';
 import Product from '../../components/Product/Product';
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
-    
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
-            setProducts(data)
-        }
-        fetchProducts()
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
     return (
         <>
             <h3> Latest Products </h3>   
-            <Row>
-                {
-                    products.map((product) => (
-                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                            <Product product={product} />
-                        </Col>
-                    ))
-                }
-            </Row>
+            {
+                loading ? <h3> loading </h3> : error ? <h3> {error} </h3> :
+                    <Row>
+                        {
+                             products.map((product) => (
+                                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                    <Product product={product} />
+                                </Col>
+                            ))
+                        }
+                    </Row>
+            }
+            
         </>
     );
 };
