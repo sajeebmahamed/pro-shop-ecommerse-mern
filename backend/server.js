f45d18bb22b25832aebfe3dd55e1a7ade09166e8
Sajeeb <sajeebahamed6@gmail.com>
2020-12-18 17:53:06 +0600

    colors npm added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const products = require('./data/products')
 const connectDB = require('./config/db')
+const color = require('colors')
 
 dotenv.config()
 connectDB()
@@ -15,4 +16,4 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 })
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`server is running ${process.env.NODE_ENV} to the port ${PORT}`))
+app.listen(PORT, console.log(`server is running ${process.env.NODE_ENV} to the port ${PORT}`.yellow.bold))
