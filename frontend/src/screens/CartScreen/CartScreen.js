c0f3362d0a4f135b127885084a653a6400982369
Sajeeb <sajeebahamed6@gmail.com>
2020-12-24 01:52:27 +0600

    add to cart done

--- frontend/src/screens/CartScreen/CartScreen.js
+++ frontend/src/screens/CartScreen/CartScreen.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from '../../actions/cartActions';
+const CartScreen = ({ match, location, history }) => {
+    const productId = match.params.id 
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+    const dispatch = useDispatch()
 
-const CartScreen = () => {
+    const cart = useSelector(state => state.cart)
+    const { cartItems } = cart
+    console.log(cartItems);
+    useEffect(() => {
+        if(productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
     return (
         <div>
             Cart
