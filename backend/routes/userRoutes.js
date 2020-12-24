94ef7a4f785a85a1b6532718f18b86da84125bff
Sajeeb <sajeebahamed6@gmail.com>
2020-12-25 02:49:46 +0600

    authentication with jsw

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const { json } = require('express')
 const express = require('express')
-const { authUser } = require('../controllers/userController')
+const { authUser, getUserProfile } = require('../controllers/userController')
+const protect = require('../middleware/authMiddleware')
 const router = require('express').Router()
 
 router.post('/login', authUser)
-
+router.route('/profile').get(protect , getUserProfile)
 module.exports = router
