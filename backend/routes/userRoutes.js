87ba1b0f838da80341fa1cf5ecfae50c72e9bc64
Sajeeb <sajeebahamed6@gmail.com>
2020-12-25 03:17:00 +0600

    user registration and pass encription done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const { json } = require('express')
 const express = require('express')
-const { authUser, getUserProfile } = require('../controllers/userController')
+const { authUser, getUserProfile, registerUser } = require('../controllers/userController')
 const protect = require('../middleware/authMiddleware')
 const router = require('express').Router()
 
+router.route('/').post(registerUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect , getUserProfile)
 module.exports = router
