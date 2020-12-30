d241996a48b048b919bbffd6eaf3b1a392a7a73c
Sajeeb <sajeebahamed6@gmail.com>
2020-12-31 03:19:38 +0600

    upload product image with multer done

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
@@ -5,7 +6,8 @@ const color = require('colors')
 const productRoutes = require('./routes/productRoutes')
 const userRoutes = require('./routes/userRoutes')
 const { notFound, errorHandler } = require('./middleware/errorMiddleware')
-const  orderRoutes = require('./routes//orderRoutes')
+const orderRoutes = require('./routes/orderRoutes')
+const uploadRoutes = require('./routes/uploadRoutes')
 
 dotenv.config()
 connectDB()
@@ -16,10 +18,14 @@ app.use(express.json())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
     res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const dirname = path.resolve()
+app.use('/uploads', express.static(path.join(dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
--- frontend/src/screens/ProductEditScreen/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen/ProductEditScreen.js
@@ -1,3 +1,4 @@
+import Axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
@@ -18,6 +19,7 @@ const ProductEditScreen = ({ match, history }) => {
     const [category, setCategory] = useState('')
     const [countInStock, setCountInStock] = useState(0)
     const [description, setDescription] = useState('')
+    const [uploading, setUploading] = useState(false)
 
     const dispatch = useDispatch()
 
@@ -49,6 +51,29 @@ const ProductEditScreen = ({ match, history }) => {
         }
 
     }, [dispatch, history, productId, product, successUpdate])
+
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0]
+        const formData = new FormData()
+        formData.append('image', file)
+        setUploading(true)
+
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            }
+
+            const { data } = await Axios.post('/api/upload', formData, config)
+
+            setImage(data)
+            setUploading(false)
+        } catch (error) {
+            console.error(error)
+            setUploading(false)
+        }
+    }
 
     const submitHandler = (e) => {
         e.preventDefault()
@@ -109,6 +134,13 @@ const ProductEditScreen = ({ match, history }) => {
                                         value={image}
                                         onChange={(e) => setImage(e.target.value)}
                                     ></Form.Control>
+                                    <Form.File
+                                        id='image-file'
+                                        label='Choose File'
+                                        custom
+                                        onChange={uploadFileHandler}
+                                    ></Form.File>
+                                    {uploading && <SpinLoader />}
                                 </Form.Group>
 
                                 <Form.Group controlId='brand'>
